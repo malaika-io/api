ff84b002193f2c4ee49e21af623370fc296a4437
karimmhamdi <contact@nourlabs.fr>
2020-03-12 18:56:23 +0100

    correction

--- lib/config/database.ts
+++ lib/config/database.ts
@@ -1,6 +1,6 @@
 import {Sequelize} from "sequelize";
 
-export const database = new Sequelize('malaika', 'root', 'libheros', {
+export const database = new Sequelize('malaika', 'root', 'Lyna2009', {
     host: 'localhost',
     dialect: 'mysql'
 });
